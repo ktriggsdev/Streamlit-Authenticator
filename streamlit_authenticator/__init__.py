597521a1648f1c587c58de760bb2a576f7b61a3b
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-04-22 14:13:15 +0100

    Update __init__.py

--- streamlit_authenticator/__init__.py
+++ streamlit_authenticator/__init__.py
@@ -26,13 +26,13 @@ if not _RELEASE:
 
     # creating a login widget
     name, authentication_status, username = authenticator.login('Login', 'main')
-    if authentication_status:
+    if st.session_state['authentication_status']:
         authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{name}*')
+        st.write(f'Welcome *{st.session_state["name"]}*')
         st.title('Some content')
-    elif authentication_status is False:
+    elif st.session_state['authentication_status'] is False:
         st.error('Username/password is incorrect')
-    elif authentication_status is None:
+    elif st.session_state['authentication_status'] is None:
         st.warning('Please enter your username and password')
 
     # Creating a password reset widget
